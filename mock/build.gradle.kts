a997a687dffb5ef7ff240d67a61649fa4bf6973e
Norbert Kiesel <nkiesel@metricstream.com>
2021-03-09 01:55:54 -0800

    upgraded 3rd party dependencies

--- mock/build.gradle.kts
+++ mock/build.gradle.kts
@@ -3,9 +3,9 @@ dependencies {
     implementation("org.slf4j:slf4j-api:1.7.30")
     implementation("ch.qos.logback:logback-classic:1.2.3")
     implementation("com.opencsv:opencsv:5.3")
-    implementation("org.mockito:mockito-core:3.6.0")
+    implementation("org.mockito:mockito-core:3.8.0")
 
     testImplementation("org.hamcrest:hamcrest:2.2")
-    testImplementation("org.junit.jupiter:junit-jupiter-api:5.7.0")
-    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.7.0")
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.7.1")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.7.1")
 }
